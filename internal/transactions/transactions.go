4ba97c993ed17c2058ec3a761a3e3b42b851700b
Patrick Jusic <patrick-96@live.it>
2019-01-14 00:15:33 +0100

    Implements transactions package
    Package based on btcutil.Tx and wire.MsgTx structure. I didn't need methods to extract transactions from []byte, I already have transaction parsed from blocks package

--- internal/transactions/transactions.go
+++ internal/transactions/transactions.go
@@ -1,30 +1,46 @@
-package tx
+package txs
 
 import (
+	"time"
+
+	"github.com/xn3cr0nx/bitgodine_code/pkg/logger"
+
 	"github.com/btcsuite/btcd/chaincfg/chainhash"
 	"github.com/btcsuite/btcutil"
+	"github.com/xn3cr0nx/bitgodine_code/internal/visitor"
 )
 
-type Tx struct {
-	btcutil.Tx
-	Version    uint32
-	TxID       chainhash.Hash
-	TxInCount  uint64
-	TxOutCount uint64
-	Locktime   uint32
-	Slice      *[]uint8
-}
+// Walk parses the btcutil.Tx object
+func Walk(tx *btcutil.Tx, v *visitor.BlockchainVisitor, timestamp time.Time, height uint64, blockItem *visitor.BlockItem, outputItems *map[chainhash.Hash][]visitor.OutputItem) btcutil.Tx {
+	transactionItem := (*v).VisitTransactionBegin(blockItem)
 
-type TxInput struct {
-	PrevHash   *chainhash.Hash
-	PrevIndex  uint32
-	Script     []byte
-	SequenceNo uint32
-	Slice      *[]uint8
-}
+	for _, i := range tx.MsgTx().TxIn {
+		var outputItem visitor.OutputItem
+		if occupied, ok := (*outputItems)[(*i).PreviousOutPoint.Hash]; ok {
+			outputItem = occupied[(*i).PreviousOutPoint.Index]
+			occupied = append(occupied[:(*i).PreviousOutPoint.Index], occupied[(*i).PreviousOutPoint.Index+1:]...)
+			// delete(*outputItems, (*i).PreviousOutPoint.Hash)
+			// 	if len(occupied) == 0 {
+			// 		// occupied.remove()
+			// 	}
+		}
+		(*v).VisitTransactionInput(*i, blockItem, &transactionItem, outputItem)
+	}
 
-type TxOutput struct {
-	Value  uint64
-	Script []byte
-	Slice  *[]uint8
+	curOutputItems := make([]visitor.OutputItem, len(tx.MsgTx().TxOut))
+	for n, o := range tx.MsgTx().TxOut {
+		outputItem, err := (*v).VisitTransactionOutput(*o, blockItem, &transactionItem)
+		if err != nil {
+			logger.Error("Transactions", err, logger.Params{"output value": string((*o).Value), "output pkscript": string((*o).PkScript)})
+			return btcutil.Tx{}
+		}
+		curOutputItems[n] = outputItem
+	}
+
+	if len(curOutputItems) > 0 {
+		(*outputItems)[*tx.Hash()] = curOutputItems
+	}
+
+	(*v).VisitTransactionEnd(*tx, blockItem, transactionItem)
+	return *tx
 }

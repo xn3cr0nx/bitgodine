267bc99896e6780d3ae21beb436b3e37b2ca2ecf
Patrick Jusic <patrick-96@live.it>
2019-01-14 00:13:57 +0100

    Completes Walk and WalkSlice methods

--- internal/blockchain/blockchain.go
+++ internal/blockchain/blockchain.go
@@ -7,11 +7,13 @@ import (
 	"path/filepath"
 
 	"github.com/btcsuite/btcd/chaincfg"
+	"github.com/btcsuite/btcd/chaincfg/chainhash"
 	"github.com/btcsuite/btcutil"
-	"github.com/xn3cr0nx/bitgodine/pkg/buffer"
-	"github.com/xn3cr0nx/bitgodine/pkg/logger"
+	"github.com/xn3cr0nx/bitgodine_code/internal/blocks"
+	"github.com/xn3cr0nx/bitgodine_code/internal/visitor"
+	"github.com/xn3cr0nx/bitgodine_code/pkg/logger"
 
-	"github.com/edsrzf/mmap-go"
+	mmap "github.com/edsrzf/mmap-go"
 	dir "github.com/mitchellh/go-homedir"
 )
 
@@ -61,61 +63,98 @@ func (b *Blockchain) Read() error {
 	return nil
 }
 
-// func (b *Blockchain) Walk(v visitor.BlockchainVisitor) (uint64, *chainhash.Hash, map[chainhash.Hash][]visitor.OutputItem) {
-// 	var skipped map[chainhash.Hash]btcutil.Block
-// 	var outputItems map[chainhash.Hash][]visitor.OutputItem
-// 	goalPrevHash, _ := chainhash.NewHash(make([]byte, 32))
-// 	var lastBlock btcutil.Block
-// 	height := uint64(0)
+func (b *Blockchain) Walk(v visitor.BlockchainVisitor) (uint64, *chainhash.Hash, map[chainhash.Hash][]visitor.OutputItem) {
+	var skipped map[chainhash.Hash]btcutil.Block
+	var outputItems map[chainhash.Hash][]visitor.OutputItem
+	goalPrevHash, _ := chainhash.NewHash(make([]byte, 32))
+	var lastBlock btcutil.Block
+	height := uint64(0)
 
-// 	for k, value := range b.Maps {
-// 		logger.Info("Blockchain", "Parsing the blockchain", logger.Params{"file": fmt.Sprintf("%v/%v", k, len(b.Maps)-1)})
-// 		b.WalkSlice(value, &goalPrevHash, &lastBlock, &height, &skipped, &outputItems, v)
-// 	}
-
-// 	return height, goalPrevHash, outputItems
-// }
-
-// func (b *Blockchain) WalkSlice(slice []uint8, goalPrevHash chainhash.Hash, lastBlock btcutil.Block, height *uint64, skipped map[chainhash.Hash]btcutil.Block, outputItems map[chainhash.Hash][]visitor.OutputItem, v visitor.BlockchainVisitor) {
-// 	for len(slice) > 0 {
-// 		if _, ok := skipped[goalPrevHash]; ok {
-// 			lastBlock.Walk(visitor, &height, outputItems)
-// 		}
-// 	}
-// }
-
-func ReadBlock(slice *[]uint8) (*btcutil.Block, error) {
-	for len(*slice) > 0 && (*slice)[0] == 0 {
-		*slice = (*slice)[1:]
+	for k, value := range b.Maps {
+		logger.Info("Blockchain", "Parsing the blockchain", logger.Params{"file": fmt.Sprintf("%v/%v", k, len(b.Maps)-1)})
+		val := []uint8(value) // I need to cast mmap.Map initializing a variable to be able to take its address (&)
+		b.WalkSlice(&val, goalPrevHash, &lastBlock, &height, &skipped, &outputItems, &v)
 	}
-	if len(*slice) == 0 {
-		return nil, errors.New("Cannot read block from slice")
-	}
-	blockMagic, err := buffer.ReadUint32(slice)
-	if err != nil {
-		return nil, errors.New("Cannot read block magic")
-	}
-	switch blockMagic {
-	case 0x00:
-		return nil, errors.New("Incomplete blk file")
-	case 0xd9b4bef9:
-		size, err := buffer.ReadUint32(slice)
+
+	return height, goalPrevHash, outputItems
+}
+
+func (b *Blockchain) WalkSlice(slice *[]uint8, goalPrevHash *chainhash.Hash, lastBlock *btcutil.Block, height *uint64, skipped *map[chainhash.Hash]btcutil.Block, outputItems *map[chainhash.Hash][]visitor.OutputItem, v *visitor.BlockchainVisitor) {
+	for len(*slice) > 0 {
+		if _, ok := (*skipped)[*goalPrevHash]; ok {
+			blocks.Walk(lastBlock, v, height, outputItems)
+			logger.Info("Blockchain", fmt.Sprintf("(rewind - pre-step) Block %v - %v -> %v", height, lastBlock.MsgBlock().Header.PrevBlock, lastBlock.Hash()), logger.Params{})
+			*height++
+			// Here I should do the for loop removing every goal_prev_hash and
+			// walking the block obtained at the index of goal_prev_hash
+			for block, ok := (*skipped)[*goalPrevHash]; ok; {
+				delete(*skipped, *goalPrevHash)
+				blocks.Walk(lastBlock, v, height, outputItems)
+				logger.Info("Blockchain", fmt.Sprintf("(rewind) Block %v - %v -> %v", height, lastBlock.MsgBlock().Header.PrevBlock, lastBlock.Hash()), logger.Params{})
+				*height++
+				goalPrevHash = block.Hash()
+				// possible bug initialization to null (None in rust)
+				*lastBlock = btcutil.Block{}
+			}
+		}
+
+		block, err := blocks.Read(slice)
 		if err != nil {
-			return nil, errors.New("Cannot read block size")
+			logger.Error("Blockchain", err, logger.Params{})
+			if len(*slice) != 0 {
+				logger.Panic("Blockchain", errors.New("Block not found but the slice of blocks is not empty"), logger.Params{})
+			}
+			break
 		}
-		if size < 80 {
-			return nil, errors.New("Cannot parse block")
+
+		logger.Info("Blockchain", fmt.Sprintf("Block candidate for height %v - goal_prev_hash = %v, prev_hash = %v, cur_hash = %v", *height, goalPrevHash.String(), block.MsgBlock().Header.PrevBlock, block.Hash()), logger.Params{})
+
+		if block.MsgBlock().Header.PrevBlock != *goalPrevHash {
+			(*skipped)[block.MsgBlock().Header.PrevBlock] = *block
+
+			// check if last_block.is_some() condition is correctly replaced with checkBlock()
+			if blocks.CheckBlock(lastBlock) && block.MsgBlock().Header.PrevBlock == lastBlock.MsgBlock().Header.PrevBlock {
+				logger.Info("Blockchain", fmt.Sprintf("Chain split detected: %v <-> %v. Detecting main chain and orphan.", lastBlock.Hash(), block.Hash()), logger.Params{})
+
+				firstOrphan := lastBlock
+				secondOrphan := block
+
+				for {
+					block, err := blocks.Read(slice)
+					if err != nil {
+						logger.Error("Blockchain", err, logger.Params{})
+						if len(*slice) != 0 {
+							logger.Panic("Blockchain", errors.New("Block not found but the slice of blocks is not empty"), logger.Params{})
+						}
+						break
+					}
+
+					(*skipped)[block.MsgBlock().Header.PrevBlock] = *block
+					if block.MsgBlock().Header.PrevBlock == *firstOrphan.Hash() {
+						// First wins
+						logger.Info("Blockchain", fmt.Sprintf("Chain split: %v is on the main chain!", firstOrphan.Hash()), logger.Params{})
+						break
+					}
+					if block.MsgBlock().Header.PrevBlock == *secondOrphan.Hash() {
+						// Second wins
+						logger.Info("Blockchain", fmt.Sprintf("Chain split: %v is on the main chain!", secondOrphan.Hash()), logger.Params{})
+						goalPrevHash = secondOrphan.Hash()
+						*lastBlock = *secondOrphan
+						break
+					}
+				}
+			}
+			continue
 		}
-		block, err := buffer.ReadSlice(slice, uint(size))
-		if err != nil {
-			return nil, errors.New("Cannot parse block")
+
+		// TODO: Here add too the check to be sure the block is correct
+		if blocks.CheckBlock(lastBlock) {
+			blocks.Walk(lastBlock, v, height, outputItems)
+			logger.Info("Blockchain", fmt.Sprintf("(last_block) Block %v - %v -> %v", height, lastBlock.MsgBlock().Header.PrevBlock, lastBlock.Hash()), logger.Params{})
+			*height++
 		}
-		res, err := btcutil.NewBlockFromBytes(block)
-		if err != nil {
-			return nil, errors.New("Cannot parse block")
-		}
-		return res, nil
-	default:
-		return nil, errors.New("No magic bytes matching")
+
+		goalPrevHash = block.Hash()
+		*lastBlock = *block
 	}
 }

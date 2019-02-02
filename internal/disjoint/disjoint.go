00afd831955308513695760312c638f61c58a418
Patrick Jusic <patrick-96@live.it>
2019-02-02 17:41:03 +0100

    Moves parsing on tx in/out to their own function and generalize disjoint
    Hashmap key type becomes interface{} from visitor.OutputItem

--- internal/disjoint/disjoint.go
+++ internal/disjoint/disjoint.go
@@ -3,78 +3,76 @@ package disjoint
 import (
 	"errors"
 
-	"github.com/xn3cr0nx/bitgodine_code/internal/visitor"
-
 	"github.com/xn3cr0nx/bitgodine_code/pkg/logger"
 )
 
 type DisjointSet struct {
-	SetSize uint
-	Parent  []uint
-	rank    []uint
-	HashMap map[visitor.OutputItem]uint
+	SetSize int
+	Parent  []int
+	Rank    []int
+	HashMap map[interface{}]int
 }
 
 func NewDisjointSet() *DisjointSet {
-	const CAPACITY int = 1000000
+	// const CAPACITY int = 1000000
 	return &DisjointSet{
 		SetSize: 0,
-		Parent:  make([]uint, CAPACITY),
-		rank:    make([]uint, CAPACITY),
-		HashMap: make(map[visitor.OutputItem]uint, CAPACITY),
+		Parent:  []int{},
+		Rank:    []int{},
+		HashMap: map[interface{}]int{},
 	}
 }
 
-func (d *DisjointSet) Size() uint {
+func (d *DisjointSet) Size() int {
 	return d.SetSize
 }
 
-func (d *DisjointSet) MakeSet(x visitor.OutputItem) {
+func (d *DisjointSet) MakeSet(x interface{}) {
 	if _, ok := d.HashMap[x]; ok {
 		return
 	}
 
 	d.HashMap[x] = d.SetSize
 	d.Parent = append(d.Parent, d.SetSize)
-	d.rank = append(d.rank, 0)
+	d.Rank = append(d.Rank, 0)
 	d.SetSize = d.SetSize + 1
 }
 
-func (d *DisjointSet) Find(x visitor.OutputItem) (uint, error) {
+func (d *DisjointSet) Find(x interface{}) (int, error) {
 	pos, ok := d.HashMap[x]
 	if !ok {
-		return 0, errors.New("Address not found")
+		return 0, errors.New("Element not found")
 	}
 	return d.FindInternal(d.Parent, pos), nil
 }
 
-func (d *DisjointSet) FindInternal(p []uint, n uint) uint {
+func (d *DisjointSet) FindInternal(p []int, n int) int {
 	if p[n] != n {
-		Parent := p[n]
-		p[n] = d.FindInternal(p, Parent)
+		parent := p[n]
+		p[n] = d.FindInternal(p, parent)
 		return p[n]
 	}
 	return n
 }
 
-func (d *DisjointSet) Union(x, y visitor.OutputItem) (uint, error) {
-	var xRoot uint
-	var yRoot uint
-	var xRank uint
-	var yRank uint
+func (d *DisjointSet) Union(x, y interface{}) (int, error) {
+	var xRoot int
+	var yRoot int
+	var xRank int
+	var yRank int
 
 	xRoot, err := d.Find(x)
 	if err != nil {
-		logger.Error("Finding in Union", err, logger.Params{})
+		logger.Error("Disjoint Set", err, logger.Params{})
 		return 0, err
 	}
-	xRank = d.rank[xRoot]
+	xRank = d.Rank[xRoot]
 	yRoot, err = d.Find(y)
 	if err != nil {
-		logger.Error("Finding in Union", err, logger.Params{})
+		logger.Error("Disjoint Set", err, logger.Params{})
 		return 0, err
 	}
-	yRank = d.rank[yRoot]
+	yRank = d.Rank[yRoot]
 
 	if xRoot == yRoot {
 		return xRoot, nil
@@ -82,17 +80,16 @@ func (d *DisjointSet) Union(x, y visitor.OutputItem) (uint, error) {
 	if xRank > yRank {
 		d.Parent[yRoot] = xRoot
 		return xRoot, nil
-	} else {
-		d.Parent[xRoot] = yRoot
-		if xRank == yRank {
-			d.rank[yRoot]++
-		}
-		return yRoot, nil
 	}
+	d.Parent[xRoot] = yRoot
+	if xRank == yRank {
+		d.Rank[yRoot]++
+	}
+	return yRoot, nil
 }
 
 func (d *DisjointSet) Finalize() {
-	for i := uint(0); i < d.SetSize; i++ {
+	for i := 0; i < d.SetSize; i++ {
 		d.FindInternal(d.Parent, i)
 	}
 }
--- internal/transactions/transactions.go
+++ internal/transactions/transactions.go
@@ -22,7 +22,18 @@ func emptySlice(arr *[]visitor.Utxo) bool {
 // Walk parses the btcutil.Tx object
 func Walk(tx *btcutil.Tx, v *visitor.BlockchainVisitor, timestamp time.Time, height uint64, blockItem *visitor.BlockItem, utxoSet *map[chainhash.Hash][]visitor.Utxo) btcutil.Tx {
 	transactionItem := (*v).VisitTransactionBegin(blockItem)
+	parseTxIn(tx, v, blockItem, utxoSet, &transactionItem)
+	err := parseTxOut(tx, v, blockItem, utxoSet, &transactionItem)
+	if err != nil {
+		logger.Error("Transactions", err, logger.Params{"tx": tx.Hash().String()})
+		return btcutil.Tx{}
+	}
+	(*v).VisitTransactionEnd(*tx, blockItem, &transactionItem)
+	return *tx
+}
 
+// Read the tx inputs removing them from related utxo set. The tx is deleted from utxo set when all outputs are spent
+func parseTxIn(tx *btcutil.Tx, v *visitor.BlockchainVisitor, blockItem *visitor.BlockItem, utxoSet *map[chainhash.Hash][]visitor.Utxo, transactionItem *visitor.TransactionItem) {
 	for _, i := range tx.MsgTx().TxIn {
 		var utxo visitor.Utxo
 		if occupied, ok := (*utxoSet)[(*i).PreviousOutPoint.Hash]; ok {
@@ -32,23 +43,22 @@ func Walk(tx *btcutil.Tx, v *visitor.BlockchainVisitor, timestamp time.Time, hei
 				delete(*utxoSet, (*i).PreviousOutPoint.Hash)
 			}
 		}
-		(*v).VisitTransactionInput(*i, blockItem, &transactionItem, utxo)
+		(*v).VisitTransactionInput(*i, blockItem, transactionItem, utxo)
 	}
+}
 
+// Creates a new set of utxo to append to the global utxo set (utxoSet)
+func parseTxOut(tx *btcutil.Tx, v *visitor.BlockchainVisitor, blockItem *visitor.BlockItem, utxoSet *map[chainhash.Hash][]visitor.Utxo, transactionItem *visitor.TransactionItem) error {
 	curUtxoSet := make([]visitor.Utxo, len(tx.MsgTx().TxOut))
 	for n, o := range tx.MsgTx().TxOut {
-		utxo, err := (*v).VisitTransactionOutput(*o, blockItem, &transactionItem)
+		utxo, err := (*v).VisitTransactionOutput(*o, blockItem, transactionItem)
 		if err != nil {
-			logger.Error("Transactions", err, logger.Params{"tx": tx.Hash().String(), "output value": string((*o).Value)})
-			return btcutil.Tx{}
+			return err
 		}
 		curUtxoSet[n] = utxo
 	}
-
 	if len(curUtxoSet) > 0 {
 		(*utxoSet)[*tx.Hash()] = curUtxoSet
 	}
-
-	(*v).VisitTransactionEnd(*tx, blockItem, &transactionItem)
-	return *tx
+	return nil
 }

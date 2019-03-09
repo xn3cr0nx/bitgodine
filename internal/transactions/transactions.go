162062d4c62a81dbe279c8d162e09e5c2403081e
Patrick Jusic <patrick-96@live.it>
2019-03-09 15:22:51 +0100

    Ends up transactions package and its unit tests

--- internal/transactions/transactions.go
+++ internal/transactions/transactions.go
@@ -1,8 +1,13 @@
 package txs
 
 import (
+	"errors"
+	"fmt"
+
 	"github.com/btcsuite/btcd/chaincfg/chainhash"
 	"github.com/btcsuite/btcutil"
+	"github.com/xn3cr0nx/bitgodine_code/internal/db"
+	"github.com/xn3cr0nx/bitgodine_code/internal/dgraph"
 )
 
 // Tx transaction type
@@ -16,126 +21,81 @@ func (tx *Tx) IsCoinbase() bool {
 	return tx.MsgTx().TxIn[0].PreviousOutPoint.Hash.IsEqual(zeroHash)
 }
 
-// // Store creates a new bucket named with the transaction id and fills it with the corresponding block hash
-// // and spent transactions outputs mapped as previous output (txid) and index (vout)
-// func (tx *Tx) Store(leveldb *database.DB, blockHash *chainhash.Hash, blockHeight int32) error {
-// 	err := (*leveldb).Update(func(t database.Tx) error {
-// 		txBucket, err := t.Metadata().CreateBucketIfNotExists([]byte(tx.Hash().String()))
-// 		if err != nil {
-// 			return err
-// 		}
+// GetSpentTx returnes the spent transaction corresponding to the index
+// passed between input transactions
+func (tx *Tx) GetSpentTx(index uint32) (Tx, error) {
+	if len(tx.MsgTx().TxIn)-1 < int(index) {
+		return Tx{}, errors.New("Index out of range in transaction input")
+	}
+	hash := tx.MsgTx().TxIn[index].PreviousOutPoint.Hash
+	hashString := hash.String()
+	node, err := dgraph.GetTx("hash", &hashString)
+	if err != nil {
+		return Tx{}, err
+	}
+	blockHash, err := chainhash.NewHashFromStr(node.Block)
+	if err != nil {
+		return Tx{}, err
+	}
+	block, err := db.GetBlock(blockHash)
+	if err != nil {
+		return Tx{}, err
+	}
+	var transaction *btcutil.Tx
+	for _, t := range block.Transactions() {
+		if t.Hash().IsEqual(&hash) {
+			transaction = t
+		}
+	}
+	return Tx{Tx: *transaction}, nil
+}
 
-// 		// if err := txBucket.Put([]byte(strconv.Itoa(int(blockHeight))), []byte(blockHash.String())); err != nil {
-// 		if err := txBucket.Put([]byte("block"), []byte(blockHash.String())); err != nil {
-// 			return err
-// 		}
+// IsSpent returnes true if exists a transaction that takes as input to the new tx
+// the output corresponding to the index passed to the function
+func (tx *Tx) IsSpent(index uint32) bool {
+	hashString := tx.Hash().String()
+	_, err := dgraph.GetFollowingTx(&hashString, &index)
+	if err != nil {
+		fmt.Println("error", err)
+		// just for sake of clarity, untill I'm going to refactor this piece to be more useful
+		if err.Error() == "transaction not found" {
+			return false
+		}
+		return false
+	}
+	return true
+}
 
-// 		for _, txIn := range tx.MsgTx().TxIn {
-// 			if err := txBucket.Put([]byte(txIn.PreviousOutPoint.Hash.String()), []byte(strconv.Itoa(int(txIn.PreviousOutPoint.Index)))); err != nil {
-// 				return err
-// 			}
-// 		}
-
-// 		return nil
-// 	})
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	return nil
-// }
-
-// // GetTxInputs extracts from the transaction stored the list of map between transaction hash and spent index
-// func GetTxInputs(leveldb *database.DB, hash *chainhash.Hash) (map[chainhash.Hash]int32, error) {
-// 	var txInputs map[chainhash.Hash]int32
-
-// 	err := (*leveldb).View(func(t database.Tx) error {
-// 		txBucket := t.Metadata().Bucket([]byte(hash.String()))
-// 		if txBucket == nil {
-// 			return errors.New("Tx not found in DB")
-// 		}
-
-// 		err := txBucket.ForEach(func(k, v []byte) error {
-// 			if string(k) != "block" {
-// 				hash, err := chainhash.NewHash(k)
-// 				if err != nil {
-// 					return err
-// 				}
-// 				vout, err := strconv.Atoi(string(v))
-// 				if err != nil {
-// 					return err
-// 				}
-// 				txInputs[*hash] = int32(vout)
-// 				if err != nil {
-// 					return err
-// 				}
-// 			}
-// 			return nil
-// 		})
-// 		if err != nil {
-// 			return err
-// 		}
-// 		return nil
-// 	})
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	return txInputs, nil
-// }
-
-// // GetTxBlock extracts the block hash the transaction in contained in
-// func GetTxBlock(leveldb *database.DB, hash *chainhash.Hash) (chainhash.Hash, error) {
-// 	var loadedBlockBytes []byte
-
-// 	err := (*leveldb).View(func(t database.Tx) error {
-// 		txBucket := t.Metadata().Bucket([]byte(hash.String()))
-// 		if txBucket == nil {
-// 			return errors.New("Tx not found in DB")
-// 		}
-
-// 		cursor := txBucket.Cursor()
-// 		if first := cursor.First(); first == false {
-// 			return nil
-// 		}
-
-// 		blockBytes := cursor.Value()
-
-// 		loadedBlockBytes = make([]byte, len(blockBytes))
-// 		copy(loadedBlockBytes, blockBytes)
-
-// 		return nil
-// 	})
-// 	if err != nil {
-// 		return chainhash.Hash{}, err
-// 	}
-
-// 	blockHash, err := chainhash.NewHash(loadedBlockBytes)
-// 	if err != nil {
-// 		return chainhash.Hash{}, err
-// 	}
-
-// 	return *blockHash, nil
-// }
-
-// func GetSpentTx(txIn *wire.TxIn) (Tx, error) {
-// 	leveldb, _ := db.LevelDB(nil)
-// 	blockHash, err := GetTxBlock(leveldb, &txIn.PreviousOutPoint.Hash)
-// 	if err != nil {
-// 		return Tx{}, err
-// 	}
-
-// 	block, err := blocks.Get(leveldb, &blockHash)
-// 	if err != nil {
-// 		return Tx{}, err
-// 	}
-
-// 	transactions := block.Transactions()
-// 	for _, t := range transactions {
-// 		if t.Hash().IsEqual(&txIn.PreviousOutPoint.Hash) {
-// 			return Tx{Tx: *t}, nil
-// 		}
-// 	}
-
-// 	return Tx{}, nil
-// }
+// GetSpendingTx returns the transaction spending the output tx of the transaction
+// passed by its index as argument
+func (tx *Tx) GetSpendingTx(index uint32) (Tx, error) {
+	if len(tx.MsgTx().TxOut)-1 < int(index) {
+		return Tx{}, errors.New("Index out of range in transaction input")
+	}
+	hash := tx.Hash()
+	hashString := hash.String()
+	node, err := dgraph.GetFollowingTx(&hashString, &index)
+	if err != nil {
+		return Tx{}, err
+	}
+	blockHash, err := chainhash.NewHashFromStr(node.Block)
+	if err != nil {
+		return Tx{}, err
+	}
+	block, err := db.GetBlock(blockHash)
+	if err != nil {
+		return Tx{}, err
+	}
+	var transaction *btcutil.Tx
+	for _, t := range block.Transactions() {
+		for _, i := range t.MsgTx().TxIn {
+			if i.PreviousOutPoint.Hash.IsEqual(hash) {
+				transaction = t
+			}
+		}
+	}
+	if transaction == nil {
+		return Tx{}, errors.New("something went wrong extracting the transaction")
+	}
+	return Tx{Tx: *transaction}, nil
+}
---
+++ internal/transactions/transactions_test.go
@@ -0,0 +1,104 @@
+package txs
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/btcsuite/btcd/chaincfg"
+	"github.com/btcsuite/btcutil"
+	"github.com/dgraph-io/badger"
+	"github.com/dgraph-io/dgo"
+	"github.com/xn3cr0nx/bitgodine_code/internal/blocks"
+	"github.com/xn3cr0nx/bitgodine_code/internal/db"
+	"github.com/xn3cr0nx/bitgodine_code/internal/dgraph"
+	"github.com/xn3cr0nx/bitgodine_code/pkg/logger"
+	"gopkg.in/go-playground/assert.v1"
+
+	"github.com/stretchr/testify/suite"
+)
+
+type TestTransactionsSuite struct {
+	suite.Suite
+	dgraph *dgo.Dgraph
+	db     *badger.DB
+}
+
+func (suite *TestTransactionsSuite) SetupSuite() {
+	logger.Setup()
+
+	DgConf := &dgraph.Config{
+		Host: "localhost",
+		Port: 9080,
+	}
+	suite.dgraph = dgraph.Instance(DgConf)
+	dgraph.Setup(suite.dgraph)
+
+	wd, err := os.Getwd()
+	assert.Equal(suite.T(), err, nil)
+	DbConf := &db.Config{
+		Dir: filepath.Join(wd, "..", "..", "badger"),
+	}
+	suite.db, err = db.Instance(DbConf)
+	assert.Equal(suite.T(), err, nil)
+	assert.NotEqual(suite.T(), suite.db, nil)
+
+	suite.Setup()
+}
+
+func (suite *TestTransactionsSuite) Setup() {
+	block, err := btcutil.NewBlockFromBytes(blocks.Block181Bytes)
+	assert.Equal(suite.T(), err, nil)
+
+	if !db.IsStored(block.Hash()) {
+		err := db.StoreBlock(&blocks.Block{Block: *block})
+		assert.Equal(suite.T(), err, nil)
+
+		for _, tx := range block.Transactions() {
+			err := dgraph.StoreTx(tx.Hash().String(), block.Hash().String(), tx.MsgTx().LockTime, tx.MsgTx().TxIn)
+			assert.Equal(suite.T(), err, nil)
+		}
+	}
+}
+
+func (suite *TestTransactionsSuite) TearDownSuite() {
+	(*suite.db).Close()
+}
+
+func (suite *TestTransactionsSuite) TestIsCoinbase() {
+	genesis := btcutil.NewBlock(chaincfg.MainNetParams.GenesisBlock)
+	firstCoinbase := &Tx{Tx: *genesis.Transactions()[0]}
+	assert.Equal(suite.T(), firstCoinbase.IsCoinbase(), true)
+}
+
+func (suite *TestTransactionsSuite) TestGetSpentTx() {
+	block, err := btcutil.NewBlockFromBytes(blocks.Block181Bytes)
+	assert.Equal(suite.T(), err, nil)
+	testTx := &Tx{Tx: *block.Transactions()[1]}
+	spentTx, err := testTx.GetSpentTx(0)
+	assert.Equal(suite.T(), err, nil)
+	assert.Equal(suite.T(), spentTx.Hash().String(), "f4184fc596403b9d638783cf57adfe4c75c605f6356fbc91338530e9831e9e16")
+	assert.Equal(suite.T(), spentTx.MsgTx().TxIn[0].PreviousOutPoint.Hash.String(), "0437cd7f8525ceed2324359c2d0ba26006d92d856a9c20fa0241106ee5a597c9")
+}
+
+func (suite *TestTransactionsSuite) TestIsSpent() {
+	block, err := btcutil.NewBlockFromBytes(blocks.Block181Bytes)
+	assert.Equal(suite.T(), err, nil)
+	testTx := &Tx{Tx: *block.Transactions()[1]}
+	spentTx, err := testTx.GetSpentTx(0)
+	assert.Equal(suite.T(), err, nil)
+	assert.Equal(suite.T(), spentTx.IsSpent(1), true)
+}
+
+func (suite *TestTransactionsSuite) TestGetSpendingTx() {
+	block, err := btcutil.NewBlockFromBytes(blocks.Block181Bytes)
+	assert.Equal(suite.T(), err, nil)
+	testTx := &Tx{Tx: *block.Transactions()[1]}
+	spendingTx, err := testTx.GetSpendingTx(1)
+	assert.Equal(suite.T(), err, nil)
+	assert.Equal(suite.T(), spendingTx.Hash().String(), "591e91f809d716912ca1d4a9295e70c3e78bab077683f79350f101da64588073")
+}
+
+func TestTransactions(t *testing.T) {
+	suite.Run(t, new(TestTransactionsSuite))
+}

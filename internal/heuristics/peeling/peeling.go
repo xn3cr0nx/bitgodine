b04d44897482b167c7ccf21b992670a437b13399
Patrick Jusic <patrick-96@live.it>
2019-03-09 15:41:20 +0100

    Implements peeling chain package in heuristics package and unit tests

---
+++ internal/heuristics/peeling/peeling.go
@@ -0,0 +1,56 @@
+package peeling
+
+import (
+	"errors"
+
+	txs "github.com/xn3cr0nx/bitgodine_code/internal/transactions"
+)
+
+// LikePeelingChain check the basic condition of peeling chain (2 txout and 1 txin)
+func LikePeelingChain(tx *txs.Tx) bool {
+	return len((*tx).MsgTx().TxOut) == 2 && len((*tx).MsgTx().TxIn) == 1
+}
+
+// IsPeelingChain returnes true id the transaction is part of a peeling chain
+func IsPeelingChain(tx *txs.Tx) bool {
+	if !LikePeelingChain(tx) {
+		return false
+	}
+
+	// Check if past transaction is peeling chain
+	spentTx, err := tx.GetSpentTx(0)
+	if err != nil {
+		return false
+	}
+	if LikePeelingChain(&spentTx) {
+		return true
+	}
+
+	// Check if future transaction is peeling chain
+	for i := range tx.MsgTx().TxOut {
+		index := uint32(i)
+		if tx.IsSpent(index) == false {
+			return false
+		}
+		spendingTx, err := tx.GetSpendingTx(index)
+		if err != nil {
+			return false
+		}
+		if LikePeelingChain(&spendingTx) {
+			return true
+		}
+	}
+
+	return true
+}
+
+// ChangeOutputPeelingChain returnes the change address output for the peeling chain
+func ChangeOutputPeelingChain(tx *txs.Tx) (uint32, error) {
+	if LikePeelingChain(tx) {
+		if (*tx).MsgTx().TxOut[0].Value > (*tx).MsgTx().TxOut[1].Value {
+			return 0, nil
+		}
+		return 1, nil
+	}
+	return 0, errors.New("transaction is not like peeling chain")
+}

aecac5ff7fa519d23e95cda11940dae2fb5373e2
Patrick Jusic <patrick-96@live.it>
2019-03-10 10:13:15 +0100

    Reimplements erase cmd after moving block and transaction to folders

--- cmd/bitgodine/cmd/erase.go
+++ cmd/bitgodine/cmd/erase.go
@@ -1,17 +1,3 @@
-// Copyright © 2019 NAME HERE <EMAIL ADDRESS>
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package cmd
 
 import (
@@ -20,19 +6,27 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/xn3cr0nx/bitgodine_code/cmd/bitgodine/cmd/block"
 	"github.com/xn3cr0nx/bitgodine_code/cmd/bitgodine/cmd/transaction"
-	"github.com/xn3cr0nx/bitgodine_code/pkg/logger"
 )
 
 // eraseCmd represents the erase command
 var eraseCmd = &cobra.Command{
 	Use:   "erase",
 	Short: "Removes all stored data",
-	Long:  "Erases blocks stored on leveldb and transaction graph stored in dgraph",
+	Long:  "Erases blocks stored on badger and transaction graph stored in dgraph",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("erase called")
-		transaction.TransactionCmd.Run(cmd, args)
-		block.BlockCmd.Run(cmd, args)
-		logger.Info("Erase", "Bitgodine Erased", logger.Params{})
+		transactionRmCommand, _, err := transaction.TransactionCmd.Find([]string{"rm"})
+		if err != nil {
+			fmt.Println("error", err)
+			return
+		}
+		go transactionRmCommand.Run(cmd, args)
+
+		blockRmCommand, _, err := block.BlockCmd.Find([]string{"rm"})
+		if err != nil {
+			fmt.Println("error", err)
+			return
+		}
+		blockRmCommand.Run(cmd, args)
 	},
 }
 

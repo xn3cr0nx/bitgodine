efa89296868ea30ba358c1f0d3e6394d567f0854
Patrick Jusic <patrick-96@live.it>
2019-04-07 01:01:20 +0200

    Adds backward and forward analyze sub-cmds, to apply specific heuristics

---
+++ cmd/bitgodine/analyze/forward.go
@@ -0,0 +1,66 @@
+package analyze
+
+import (
+	"errors"
+	"os"
+
+	"github.com/btcsuite/btcd/chaincfg/chainhash"
+	"github.com/olekukonko/tablewriter"
+	"github.com/spf13/cobra"
+	"github.com/xn3cr0nx/bitgodine_code/internal/heuristics/forward"
+	txs "github.com/xn3cr0nx/bitgodine_code/internal/transactions"
+	"github.com/xn3cr0nx/bitgodine_code/pkg/logger"
+)
+
+// forwardCmd represents the forward command
+var forwardCmd = &cobra.Command{
+	Use:   "forward",
+	Short: "Apply forward heuristic to transaction",
+	Long:  "",
+	Run: func(cmd *cobra.Command, args []string) {
+		if args[0] == "" {
+			logger.Panic("Analyze", errors.New("Missing transaction hash"), logger.Params{})
+		}
+
+		logger.Info("Analyze forward", "Analyzing...", logger.Params{"tx": args[0]})
+
+		txHash, err := chainhash.NewHashFromStr(args[0])
+		if err != nil {
+			logger.Panic("Analyze forward", err, logger.Params{})
+		}
+		tx, err := txs.Get(txHash)
+
+		table := tablewriter.NewWriter(os.Stdout)
+		table.SetHeader([]string{"Heuristic", "Transaction", "Privacy"})
+		table.SetBorder(false)
+
+		if forward.Vulnerable(&tx) {
+			table.SetColumnColor(
+				tablewriter.Colors{},
+				tablewriter.Colors{tablewriter.Bold, tablewriter.FgHiRedColor},
+				tablewriter.Colors{tablewriter.Bold, tablewriter.FgHiRedColor})
+			table.Append([]string{"Forward", args[0], "✓"})
+		} else {
+			table.SetColumnColor(
+				tablewriter.Colors{},
+				tablewriter.Colors{tablewriter.Bold, tablewriter.FgGreenColor},
+				tablewriter.Colors{tablewriter.Bold, tablewriter.FgGreenColor})
+			table.Append([]string{"Forward", args[0], "x"})
+		}
+
+		table.Render()
+	},
+}
+
+// func init() {
+
+// 	// Here you will define your flags and configuration settings.
+
+// 	// Cobra supports Persistent Flags which will work for this command
+// 	// and all subcommands, e.g.:
+// 	// forwardCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+// 	// Cobra supports local flags which will only run when this command
+// 	// is called directly, e.g.:
+// 	// forwardCmd.Flags().BoolP("toggle", "t", faforwarde, "Help message for toggle")
+// }

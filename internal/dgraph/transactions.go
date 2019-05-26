2622bd4afe5a6d1b396416eb8a4398189977da29
Patrick Jusic <patrickjusic96@gmail.com>
2019-05-26 10:06:57 +0200

    Updates tx queries forcing order in outputs and implements IsSpent

--- internal/dgraph/transactions.go
+++ internal/dgraph/transactions.go
@@ -93,14 +93,14 @@ func GetTx(hash string) (Transaction, error) {
 			hash
 			locktime
 			version
-			inputs {
+			inputs (orderasc: vout) {
 				uid
 				hash
 				vout
 				signature_script
 				witness
 			}
-			outputs {
+			outputs (orderasc: vout) {
 				uid
 				value
 				vout
@@ -152,7 +152,7 @@ func GetTxUID(hash *string) (string, error) {
 func GetTxOutputs(hash *string) ([]Output, error) {
 	resp, err := instance.NewTxn().Query(context.Background(), fmt.Sprintf(`{
 		transactions(func: allofterms(hash, %s)) {
-			outputs {
+			outputs (orderasc: vout) {
 				uid
         value
         vout
@@ -217,7 +217,7 @@ func GetFollowingTx(hash *string, vout *uint32) (Transaction, error) {
 				hash
 				vout
 			}
-			outputs {
+			outputs (orderasc: vout) {
 				value
 				vout
 			}
@@ -317,4 +317,18 @@ func GetTransactionsHeightRange(from, to *int32) ([]Transaction, error) {
 		}
 	}
 	return txs, nil
+}
+
+// IsSpent returnes true if exists a transaction that takes as input to the new tx
+// the output corresponding to the index passed to the function
+func IsSpent(tx string, index uint32) bool {
+	_, err := GetFollowingTx(&tx, &index)
+	if err != nil {
+		// just for sake of clarity, untill I'm going to refactor this piece to be more useful
+		if err.Error() == "transaction not found" {
+			return false
+		}
+		return false
+	}
+	return true
 }

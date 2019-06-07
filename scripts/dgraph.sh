6c84e5f627aaa26a2170b2a93b7845c7c37d8aec
Patrick Jusic <patrickjusic96@gmail.com>
2019-06-07 20:57:19 +0200

    Fixes docker-compose file to split dgraph services

--- scripts/dgraph.sh
+++ scripts/dgraph.sh
@@ -9,10 +9,10 @@ else
     # Run Dgraph Zero
     echo "Starting dgraph named diggy"
     docker run -d -p 5080:5080 -p 6080:6080 -p 8080:8080 -p 9080:9080 -p 8000:8000 -v ~/.bitgodine/dgraph:/dgraph --name diggy dgraph/dgraph dgraph zero
+    # Run Dgraph Alpha
+    docker exec -d diggy dgraph alpha --lru_mb 12288 --zero localhost:5080
+    # Run Dgraph Ratel
+    docker exec -d diggy dgraph-ratel
 fi
 
-# Run Dgraph Alpha
-docker exec -d diggy dgraph alpha --lru_mb 12288 --zero localhost:5080
 
-# Run Dgraph Ratel
-docker exec -d diggy dgraph-ratel

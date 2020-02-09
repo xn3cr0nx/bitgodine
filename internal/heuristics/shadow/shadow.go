c9f1cd37563e1406381e53cff1d71e2eaeadccf1
Patrick Jusic <patrickjusic96@gmail.com>
2020-02-09 16:43:47 +0100

    Implement shadow heuristic and testing

---
+++ internal/heuristics/shadow/shadow.go
@@ -0,0 +1,70 @@
+// Package behaviour client heuristic
+// This heuristic checks if there are
+// output addresses that are the first time they appear in the Blockchain. We
+// count the transactions in which appear at least one fresh address in the
+// output set.
+package shadow
+
+import (
+	"runtime"
+
+	"github.com/xn3cr0nx/bitgodine_parser/pkg/models"
+	"github.com/xn3cr0nx/bitgodine_parser/pkg/storage"
+	task "github.com/xn3cr0nx/bitgodine_server/internal/errtask"
+)
+
+// Worker struct implementing workers pool
+type Worker struct {
+	db          storage.DB
+	output      models.Output
+	vout        int
+	candidates  []uint32
+	blockHeight int32
+}
+
+// Work executed in the workers pool
+func (w *Worker) Work() (err error) {
+	firstOccurence, err := w.db.GetAddressFirstOccurenceHeight(w.output.ScriptpubkeyAddress)
+	if err != nil {
+		return
+	}
+	// check if occurence is the first, e.g. the transaction block height is the firstOccurence
+	if firstOccurence < w.blockHeight {
+		w.candidates[w.vout] = 1
+	}
+	return
+}
+
+// ChangeOutput returnes the index of the output which appears for the first time in the chain based on client behaviour heuristic
+// TODO: violates DRY, just different evaluation in output change, but same operations
+func ChangeOutput(db storage.DB, tx *models.Tx) (c []uint32, err error) {
+	candidates := make([]uint32, len(tx.Vout))
+	blockHeight, err := db.GetTxBlockHeight(tx.TxID)
+	if err != nil {
+		return
+	}
+
+	pool := task.New(runtime.NumCPU())
+	for vout, out := range tx.Vout {
+		if out.ScriptpubkeyAddress == "" {
+			continue
+		}
+		pool.Do(&Worker{db, out, vout, candidates, blockHeight})
+	}
+	if err = pool.Shutdown(); err != nil {
+		return
+	}
+
+	for i, v := range candidates {
+		if v == 1 {
+			c = append(c, uint32(i))
+		}
+	}
+	return
+}
+
+// Vulnerable returnes true if the transaction has a privacy vulnerability due to optimal change heuristic
+func Vulnerable(db storage.DB, tx *models.Tx) bool {
+	c, err := ChangeOutput(db, tx)
+	return err == nil && len(c) > 0
+}
---
+++ internal/heuristics/shadow/shadow_test.go
@@ -0,0 +1,59 @@
+package shadow
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+	"github.com/xn3cr0nx/bitgodine_parser/pkg/badger/kv"
+	"github.com/xn3cr0nx/bitgodine_parser/pkg/logger"
+	"github.com/xn3cr0nx/bitgodine_parser/pkg/models"
+	"github.com/xn3cr0nx/bitgodine_server/internal/test"
+)
+
+type TestAddressReuseSuite struct {
+	suite.Suite
+	db     *kv.KV
+	target models.Tx
+}
+
+func (suite *TestAddressReuseSuite) SetupSuite() {
+	logger.Setup()
+
+	db, err := test.InitDB()
+	require.Nil(suite.T(), err)
+	suite.db = db.(*kv.KV)
+
+	suite.Setup()
+}
+
+func (suite *TestAddressReuseSuite) Setup() {
+	// check blockchain is synced at least to block 1000
+	h, err := suite.db.GetLastBlockHeight()
+	require.Nil(suite.T(), err)
+	require.GreaterOrEqual(suite.T(), h, int32(1000))
+
+	tx, err := suite.db.GetTx(test.VulnerableFunctions(("Shadow")))
+	require.Nil(suite.T(), err)
+	suite.target = tx
+}
+
+func (suite *TestAddressReuseSuite) TearDownSuite() {
+	test.CleanTestDB(suite.db)
+}
+
+func (suite *TestAddressReuseSuite) TestChangeOutput() {
+	c, err := ChangeOutput(suite.db, &suite.target)
+	require.Nil(suite.T(), err)
+	assert.Equal(suite.T(), c, []uint32{uint32(1)})
+}
+
+func (suite *TestAddressReuseSuite) TestVulnerable() {
+	v := Vulnerable(suite.db, &suite.target)
+	assert.Equal(suite.T(), v, true)
+}
+
+func TestAddressReuse(t *testing.T) {
+	suite.Run(t, new(TestAddressReuseSuite))
+}

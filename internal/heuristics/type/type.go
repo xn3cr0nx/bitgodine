eb6b82181ce0eb50004e160dc0133fe99a19cf34
Patrick Jusic <patrickjusic96@gmail.com>
2020-02-09 05:36:59 +0100

    Update implementation and testing of addres type heuristic

--- internal/heuristics/type/type.go
+++ internal/heuristics/type/type.go
@@ -6,6 +6,7 @@ package class
 
 import (
 	"errors"
+
 	"golang.org/x/sync/errgroup"
 
 	"github.com/xn3cr0nx/bitgodine_parser/pkg/models"
@@ -13,11 +14,20 @@ import (
 )
 
 // ChangeOutput returnes the index of the output which address type corresponds to input addresses type
-func ChangeOutput(db storage.DB, tx *models.Tx) (uint32, error) {
+func ChangeOutput(db storage.DB, tx *models.Tx) (c []uint32, err error) {
 	inputTypes := make([]string, len(tx.Vin))
 	outputTypes := make([]string, len(tx.Vout))
 
 	var g errgroup.Group
+	g.Go(func() error {
+		for o, out := range tx.Vout {
+			outputTypes[o] = out.ScriptpubkeyType
+			if outputTypes[o] == outputTypes[0] {
+				return errors.New("Two or more output of the same type, cannot determine change output")
+			}
+		}
+		return nil
+	})
 	g.Go(func() error {
 		for i, in := range tx.Vin {
 			if in.IsCoinbase {
@@ -34,31 +44,23 @@ func ChangeOutput(db storage.DB, tx *models.Tx) (uint32, error) {
 		}
 		return nil
 	})
-	g.Go(func() error {
-		for o, out := range tx.Vout {
-			outputTypes[o] = out.ScriptpubkeyType
-			if outputTypes[o] != outputTypes[0] {
-				return errors.New("Two or more output of the same type, cannot determine change output")
-			}
-		}
-		return nil
-	})
-	if err := g.Wait(); err != nil {
-		return 0, err
+	if err = g.Wait(); err != nil {
+		return
 	}
 
 	for _, input := range inputTypes {
 		for vout, output := range outputTypes {
 			if input == output {
-				return uint32(vout), nil
+				c = append(c, uint32(vout))
 			}
 		}
 	}
-	return 0, errors.New("No address type matching input addresses type found")
+
+	return
 }
 
 // Vulnerable returnes true if the transaction has a privacy vulnerability due to optimal change heuristic
 func Vulnerable(db storage.DB, tx *models.Tx) bool {
-	_, err := ChangeOutput(db, tx)
-	return err == nil
+	c, err := ChangeOutput(db, tx)
+	return err == nil && len(c) > 0
 }
